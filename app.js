16abbf7b8928ac550ac433c667b0f7e25b780930
Jose Breiner <jbpaig@gmail.com>
2022-02-03 18:07:27 -0500

    javascript code injection in template html

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const app = Vue.createApp({
-    template: `
-        <h1>Hello world from VueJS</h1>
-        <p>this is my first web page using VueJs</p>
-
-
-        <p> Javascript code injection </p>
-        <span>{{ {"a":1,"b": 2} }}</span>
-    `,
-})
+const app = Vue.createApp({})
 
 app.mount('#app')
